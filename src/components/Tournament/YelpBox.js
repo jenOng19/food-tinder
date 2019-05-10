cc35432d68d02830527daad085b96105e98a7698
Kevin Yang <kyang812@yahoo.com>
2019-05-10 11:19:56 -0700

    merged

---
+++ src/components/Tournament/YelpBox.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class YelpBox extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            
+        }
+    }
+
+    render(){
+        return (
+            <
+        )
+    }
+}
+
+ReactDOM.render(
+    <CustomButton />,
+    document.getElementById('root')
+);
+
+export default YelpBox;
---
+++ src/components/YelpBox/YelpBox.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Yelp extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            
+        }
+    }
+}
