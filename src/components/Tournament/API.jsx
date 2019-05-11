37912c5d7779edd0d0dfc327b692a07775981f26
kate park <rudhi1009@gmail.com>
2019-05-11 15:33:35 -0700

    get data from search input

---
+++ src/components/Tournament/API.jsx
@@ -0,0 +1,43 @@
+// import React from "react";
+// import axios from "axios";
+// import API_key from "./KEYS";
+
+// // const yelp_date = axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=NYC`, {
+// //     headers: {
+// //       Authorization: `Bearer ${API_key}`
+// //     },
+// //     params: {
+// //       categories: 'breakfast_brunch',
+// //     }
+// //   })
+// //     .then((res) => {
+// //     console.log(res)
+// //     })
+// //      .catch((err) => {
+// //     console.log ('error')
+// //     })
+
+
+// function YelpDate (location) {
+//   axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${location}`, {
+//   headers: {
+//     Authorization: `Bearer ${API_key}`
+//   },
+//   params: {
+//     location: {location},
+//     categories: 'lunch',
+//   }
+//   })
+//   .then((res) => {
+//     console.log(res.data.businesses);
+//   })
+//   .catch((err) => {
+//   console.log ('error')
+//   })    
+// }
+
+// function GetYelpData(location){
+//   const yelpdata = YelpDate(loaction);
+// }
+
+// export default YelpDate;
--- src/components/Tournament/Form.jsx
+++ src/components/Tournament/Form.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import YelpDate from "./API";
+import axios from "axios";
+import API_key from "./KEYS";
 
 class Search extends React.Component {
     constructor(props) {
@@ -8,22 +10,51 @@ class Search extends React.Component {
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
+    getYelpData () {
+      const yelpDataFromApi = this.yelpDate(this.state.search);
+      this.setState({yelp : yelpDataFromApi });
+    }
+    yelpDate (location) {
+      axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${location}`, {
+      headers: {
+        Authorization: `Bearer ${API_key}`
+      },
+      params: {
+        categories: 'lunch',
+      }
+      })
+      .then((res) => {
+      console.log(res.data.businesses);
+      })
+      .catch((err) => {
+      console.log ('error')
+      })  
+    }   
+    renderYelpData () {
+      const yelpData = this.state.yelp.map( business => {
+        console.log('business :', business);
+        return <p>{business.name}</p>
+      })
+      return yelpData;
+    }
     handleChange(event) {
       this.setState({ search: event.target.value });
     }
     handleSubmit(event) {
       event.preventDefault();
       console.log('this.state :', this.state);
-      <YelpDate location ={this.state.search}/>
+      this.getYelpData();
+      this.renderYelpData();
     }
     render() {
+      const {yelp} = this.state;
       return (
         <form onSubmit={this.handleSubmit}>
           <label>
             search :
             <input type="text" value={this.state.search} onChange={this.handleChange} />
             <input type="submit" value="Submit" onSubmit={this.handleSubmit}/>
-            {/* <p>{JSON.parse(yelp_date)}</p> */}
+            <div className='yelp'>{this.state.yelp? this.renderYelpData : 'loading'}</div>
           </label>
         </form>
       );
