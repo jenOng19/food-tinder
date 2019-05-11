3042b240cbd8e5988a28db792ae356078a701d45
kate park <rudhi1009@gmail.com>
2019-05-11 16:00:17 -0700

    get names form data, display into DOM

--- src/components/Tournament/Form.jsx
+++ src/components/Tournament/Form.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import YelpDate from "./API";
 import axios from "axios";
 import API_key from "./KEYS";
 
 class Search extends React.Component {
     constructor(props) {
       super(props);
-      this.state = { search: '' };
+      this.state = { search: '', yelp: [] };
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
     getYelpData () {
-      const yelpDataFromApi = this.yelpDate(this.state.search);
-      this.setState({yelp : yelpDataFromApi });
+      this.yelpDate(this.state.search);
     }
     yelpDate (location) {
       axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${location}`, {
@@ -24,16 +22,17 @@ class Search extends React.Component {
       }
       })
       .then((res) => {
-      console.log(res.data.businesses);
+        console.log(res.data.businesses);
+        this.setState({yelp: res.data.businesses})
       })
       .catch((err) => {
-      console.log ('error')
+        console.log ('error')
       })  
     }   
     renderYelpData () {
       const yelpData = this.state.yelp.map( business => {
         console.log('business :', business);
-        return <p>{business.name}</p>
+        return <p key={business.id}>{business.name}</p>
       })
       return yelpData;
     }
@@ -47,14 +46,13 @@ class Search extends React.Component {
       this.renderYelpData();
     }
     render() {
-      const {yelp} = this.state;
       return (
         <form onSubmit={this.handleSubmit}>
           <label>
             search :
             <input type="text" value={this.state.search} onChange={this.handleChange} />
             <input type="submit" value="Submit" onSubmit={this.handleSubmit}/>
-            <div className='yelp'>{this.state.yelp? this.renderYelpData : 'loading'}</div>
+            <div className='yelp'>{this.renderYelpData()}</div>
           </label>
         </form>
       );
